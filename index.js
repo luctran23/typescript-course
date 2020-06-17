77442b7517006d04cd77f58834c334aea2e61f88
luctran23 <anhvanluc1999@gmail.com>
2020-06-17 17:49:22 +0700

    Lesson 2: Classes

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 function greeter(person) {
-    console.log("hello " + person.firstname);
+    console.log("hello " + person.firstname + " " + person.lastname);
 }
+var user = { firstname: "Johny", lastname: "Dang" };
 greeter({
     firstname: "lara",
     lastname: "joey"
 });
+greeter(user);
--- index.ts
+++ index.ts
@@ -3,10 +3,11 @@ interface Person {
     lastname: string;
 }
 function greeter(person: Person) {
-    console.log( "hello " + person.firstname);
+    console.log( "hello " + person.firstname + " " + person.lastname);
 }
-
+let user = {firstname: "Johny", lastname: "Dang"};
 greeter({
     firstname: "lara",
     lastname: "joey"
 });
+greeter(user);
