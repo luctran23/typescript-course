931f77cfba61dda5fa168a3cbf39bb93ac705cda
luctran23 <anhvanluc1999@gmail.com>
2020-06-20 05:44:10 +0700

    Define an interface Person

--- interfaces/main.ts
+++ interfaces/main.ts
@@ -1,5 +1,10 @@
-function greeter(person: {name: string}) {
+interface Person {
+    name: string;
+}
+
+function greeter(person: Person) {
     console.log("Hello, " + person.name);
 }
 let girl = {name: 'lara', age: 21};
+
 greeter(girl);
